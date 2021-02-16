75855cbe975494851aba0ede0960a0890299f129
Daniel Glucksman <daniel@grocerkey.com>
2021-02-16 05:48:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const path = require('path');
 const{ execSync } = require('child_process');
+const fs = require('fs');
 
 try {    
   const environment = core.getInput('environment');
@@ -9,10 +10,15 @@ try {
   const gitToken = core.getInput('gitToken');
   
   
-  execSync(`git clone https://${gitToken}:x-oauth-basic@github.com/GrocerKey/dev-credentials.git`, {
+  execSync(`git clone https://${gitToken}:x-oauth-basic@github.com/GrocerKey/ci-environment-variables.git`, {
   stdio: [0, 1, 2], // we need this so node will print the command output
   cwd: path.resolve(__dirname, ''), // path to where you want to save the file
   })
+  
+  let rawdata = fs.readFileSync(path.resolve(__dirname,`ci-environment-variables\${environment}-environment.txt`);
+  let parsedData = JSON.parse(rawdata);
+  console.log(parsedData);
+
 
   var variables = vars.split(' ');
 
