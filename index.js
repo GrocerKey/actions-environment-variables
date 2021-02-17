23a6cb56d4b00d55df8341ff9393dfb4797bb28c
Daniel Glucksman <daniel@grocerkey.com>
2021-02-17 08:46:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const path = require('path');
-const{ execSync } = require('child_process');
-const fs = require('fs');
 
 try {    
-  const branch = core.getInput('branch');
-  const vars = core.getInput('vars');
-  const gitToken = core.getInput('gitToken');
-  
-  const environment = getEnvironment(branch);
-  
-  execSync(`git clone https://${gitToken}:x-oauth-basic@github.com/GrocerKey/ci-environment-variables.git`, {
-  stdio: [0, 1, 2],
-  cwd: path.resolve(__dirname, ''), 
-  })
-  
-  
-  let rawdata = fs.readFileSync(path.resolve(__dirname,`ci-environment-variables\\${environment}-environment.txt`));
-  let parsedData = JSON.parse(rawdata);
-
-
-  var variables = vars.split(' ');
-  
-  variables.forEach((item) => {
-     if(parsedData.hasOwnProperty(item))
-     {
-       core.exportVariable(item, parsedData[item]);
-     }
-  });  
+  const branch = core.getInput('branch');  
+  setEnvironment(branch);
 }
 
 catch (error) {
   core.setFailed(error.message);
 }
 
-function getEnvironment(branchName) 
+function setEnvironment(branchName) 
 {
     var branchSuffix = branchName.replace('refs/heads/','').toLowerCase();
     var isFeatureBranch = false;
@@ -59,8 +34,6 @@ function getEnvironment(branchName)
 
      core.exportVariable('IS_FEATURE_BRANCH', isFeatureBranch);
      core.exportVariable('ENVIRONMENT', environment);
-
-     return environment;
    }
 
   
